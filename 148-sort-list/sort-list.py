f05bbdf5bb38636757f6f0c8d25b1643209b4dbb
Premprakash Sharma <sharmaprem8294@gmail.com>
2026-08-24 23:20:01 +0530

    Time: 185 ms (31.14%) | Memory: 40.6 MB (57.92%) - LeetSync

---
+++ 148-sort-list/sort-list.py
@@ -0,0 +1,24 @@
+class Solution:
+    def sortList(self, head):
+        if not head or not head.next:
+            return head
+
+        slow = fast = head
+        while fast.next and fast.next.next:
+            slow, fast = slow.next, fast.next.next
+        mid = slow.next
+        slow.next = None
+        left = self.sortList(head)
+        right = self.sortList(mid)
+
+        dummy = cur = ListNode(0)
+
+        while left and right:
+            if left.val < right.val:
+                cur.next, left = left, left.next
+            else:
+                cur.next, right = right, right.next
+            cur = cur.next
+
+        cur.next = left or right
+        return dummy.next
